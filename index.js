cd6520df39c869de15e7e8a51b09d9fca2d857a6
cdmmucb <carlos.mendizabal@ucb.edu.bo>
2022-04-10 16:24:26 -0400

    cors middleware

--- index.js
+++ index.js
@@ -59,6 +59,13 @@ async function main() {
   });
 }
 
+//CORS HEADERS MIDDLEWARE
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "YOUR-DOMAIN.TLD"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.listen(port, () => {
   console.log(`Tickets listening on port ${port}`);
 });
